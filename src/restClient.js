386fba9c7d582e02e131c2f08da11dbeae24b8bb
Dream <dreamrace@users.noreply.github.com>
2017-08-22 18:44:49 +0800

    fix GET_MANY missing id

--- src/restClient.js
+++ src/restClient.js
@@ -67,6 +67,7 @@ export default (client, options = {}) => {
         return {data: {...response, id: response[idKey]}}
       case CREATE:
         return {data: {...params.data, id: response[idKey]}}
+      case GET_MANY: // fix GET_MANY missing id
       case GET_LIST:
         response.data = response.data.map(item => {
           if (idKey !== 'id') {
