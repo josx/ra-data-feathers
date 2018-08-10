493856e20332413d77af489efef1ce8357f4dee2
José Luis Di Biase <josx@interorganic.com.ar>
2018-08-10 11:12:44 -0300

    Kill calling feathers authenticate for every service
    Signed-off-by: José Luis Di Biase <josx@interorganic.com.ar>

--- src/restClient.js
+++ src/restClient.js
@@ -93,8 +93,6 @@ export default (client, options = {}) => {
   }
 
   return (type, resource, params) =>
-    client.authenticate()
-        .then(() => mapRequest(type, resource, params))
-        .then(response => mapResponse(response, type, resource, params)
-    )
+    mapRequest(type, resource, params)
+      .then(response => mapResponse(response, type, resource, params))
 }
