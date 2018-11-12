856e50fb380bbc83b16eb75689b4bb0f41049e14
Santiago <sgobotta@gmail.com>
2018-11-11 21:58:12 -0300

    RestClient: fixes eslint issues in src and test

--- src/restClient.js
+++ src/restClient.js
@@ -8,104 +8,105 @@ import {
   UPDATE_MANY,
   DELETE,
   DELETE_MANY,
-  fetchUtils
-} from 'react-admin'
-import debug from 'debug'
-import diff from 'object-diff'
+  fetchUtils,
+} from 'react-admin';
+import debug from 'debug';
+import diff from 'object-diff';
 
-const dbg = debug('ra-data-feathers:rest-client')
+const dbg = debug('ra-data-feathers:rest-client');
 
-function getIdKey({resource, options}) {
-  return (options[resource] && options[resource].id) || options.id || 'id'
+function getIdKey({ resource, options }) {
+  return (options[resource] && options[resource].id) || options.id || 'id';
 }
 
 export default (client, options = {}) => {
   const usePatch = !!options.usePatch;
   const mapRequest = (type, resource, params) => {
-    const idKey = getIdKey({resource, options})
-    dbg('type=%o, resource=%o, params=%o, idKey=%o', type, resource, params, idKey)
-    const service = client.service(resource)
-    let query = {}
+    const idKey = getIdKey({ resource, options });
+    dbg('type=%o, resource=%o, params=%o, idKey=%o', type, resource, params, idKey);
+    const service = client.service(resource);
+    const query = {};
 
     switch (type) {
       case GET_MANY:
-        let ids = params.ids || []
-        query[idKey] = {$in: ids}
-        query['$limit'] = ids.length
-        return service.find({query})
+        const ids = params.ids || [];
+        query[idKey] = { $in: ids };
+        query.$limit = ids.length;
+        return service.find({ query });
       case GET_MANY_REFERENCE:
-        if(params.target && params.id){
-          query[params.target] = params.id
+        if (params.target && params.id) {
+          query[params.target] = params.id;
         }
       case GET_LIST:
-        const {page, perPage} = params.pagination || {}
-        const {field, order} = params.sort || {}
-        const sortKey = `$sort[${field === 'id' ? idKey : field}]`
-        dbg('field=%o, sort-key=%o', field, sortKey)
-        let sortVal = order === 'DESC' ? -1 : 1
+        const { page, perPage } = params.pagination || {};
+        const { field, order } = params.sort || {};
+        const sortKey = `$sort[${field === 'id' ? idKey : field}]`;
+        dbg('field=%o, sort-key=%o', field, sortKey);
+        const sortVal = order === 'DESC' ? -1 : 1;
         if (perPage && page) {
-          query['$limit'] = perPage
-          query['$skip'] = perPage * (page - 1)
+          query.$limit = perPage;
+          query.$skip = perPage * (page - 1);
         }
         if (order) {
-          query[sortKey] = JSON.stringify(sortVal)
+          query[sortKey] = JSON.stringify(sortVal);
         }
         Object.assign(query, fetchUtils.flattenObject(params.filter));
-        dbg('query=%o', query)
-        return service.find({query})
+        dbg('query=%o', query);
+        return service.find({ query });
       case GET_ONE:
-        return service.get(params.id)
+        return service.get(params.id);
       case UPDATE:
         if (usePatch) {
-          const data = params.previousData ? diff(params.previousData, params.data) : params.data
-          return service.patch(params.id, data)
+          const data = params.previousData ? diff(params.previousData, params.data) : params.data;
+          return service.patch(params.id, data);
         }
-        return service.update(params.id, params.data)
+        return service.update(params.id, params.data);
       case UPDATE_MANY:
         if (usePatch) {
-          const data = params.previousData ? diff(params.previousData, params.data) : params.data
+          const data = params.previousData ? diff(params.previousData, params.data) : params.data;
           return Promise.all(params.ids.map(id => (service.patch(id, data))));
         }
         return Promise.all(params.ids.map(id => (service.update(id, params.data))));
       case CREATE:
-        return service.create(params.data)
+        return service.create(params.data);
       case DELETE:
-        return service.remove(params.id)
+        return service.remove(params.id);
       case DELETE_MANY:
-        return Promise.all(params.ids.map(id => (service.remove(id) )));
+        return Promise.all(params.ids.map(id => (service.remove(id))));
       default:
         return Promise.reject(`Unsupported FeathersJS restClient action type ${type}`);
     }
-  }
+  };
 
   const mapResponse = (response, type, resource, params) => {
-    const idKey = getIdKey({resource, options})
+    const idKey = getIdKey({ resource, options });
     switch (type) {
       case GET_ONE:
       case UPDATE:
       case DELETE:
-        return {data: {...response, id: response[idKey]}}
+        return { data: { ...response, id: response[idKey] } };
       case UPDATE_MANY:
       case DELETE_MANY:
-        return {data: response.map(record => record[idKey])}
+        return { data: response.map(record => record[idKey]) };
       case CREATE:
-        return {data: { ...params.data, ...response, id: response[idKey]}}
+        return { data: { ...params.data, ...response, id: response[idKey] } };
       case GET_MANY_REFERENCE: // fix GET_MANY_REFERENCE missing id
       case GET_MANY: // fix GET_MANY missing id
       case GET_LIST:
-        response.data = response.data.map(item => {
+        response.data = response.data.map((_item) => {
+          const item = _item;
           if (idKey !== 'id') {
-            item.id = item[idKey]
+            item.id = _item[idKey];
           }
-          return item
-        })
-        return response
+          return _item;
+        });
+        return response;
       default:
-        return response
+        return response;
     }
-  }
+  };
 
   return (type, resource, params) =>
     mapRequest(type, resource, params)
-      .then(response => mapResponse(response, type, resource, params))
-}
+      .then(response => mapResponse(response, type, resource, params));
+};
--- test/restClient.spec.js
+++ test/restClient.spec.js
@@ -1,8 +1,3 @@
-"use strict";
-
-const expect = require('chai').expect;
-const sinon = require('sinon');
-const restClient = require('../lib').restClient;
 
 import {
   GET_MANY,
@@ -13,18 +8,21 @@ import {
   UPDATE,
   UPDATE_MANY,
   DELETE,
-  DELETE_MANY
-} from 'react-admin'
+  DELETE_MANY,
+} from 'react-admin';
 
-const debug = require('debug')
+const { expect } = require('chai');
+const sinon = require('sinon');
+const debug = require('debug');
+const { restClient } = require('../lib');
 
-const dbg = debug('ra-data-feathers:test')
+debug('ra-data-feathers:test');
 
 const findResult = {
   total: 1,
   data: [
     { id: 1 },
-  ]
+  ],
 };
 const getResult = { id: 1, title: 'gotten' };
 const updateResult = { id: 1, title: 'updated' };
@@ -32,14 +30,20 @@ const createResult = { id: 1, title: 'created' };
 const removeResult = { id: 1, title: 'deleted' };
 const authenticateResult = {};
 
-let aorClient, fakeClient, fakeService;
+let aorClient;
+let fakeClient;
+let fakeService;
 
 function setupClient(options = {}) {
   fakeService = {
     find: sinon.stub().returns(Promise.resolve(findResult)),
     get: sinon.stub().returns(Promise.resolve(getResult)),
-    update: sinon.stub().callsFake((id, data) => (Promise.resolve(Object.assign({}, data, { id: id })))),
-    patch: sinon.stub().callsFake((id, data) => (Promise.resolve(Object.assign({}, data, { id: id })))),
+    update: sinon.stub().callsFake((id, data) => Promise.resolve(
+      Object.assign({}, data, { id }),
+    )),
+    patch: sinon.stub().callsFake((id, data) => Promise.resolve(
+      Object.assign({}, data, { id }),
+    )),
     create: sinon.stub().returns(Promise.resolve(createResult)),
     remove: sinon.stub().returns(Promise.resolve(removeResult)),
   };
@@ -47,42 +51,41 @@ function setupClient(options = {}) {
   // sinon.spy(fakeService.find)
 
   fakeClient = {
-    service: (resource) => fakeService,
-    authenticate: () => Promise.resolve(authenticateResult)
+    service: () => fakeService,
+    authenticate: () => Promise.resolve(authenticateResult),
   };
 
   aorClient = restClient(fakeClient, options);
 }
 
 
-
 describe('Rest Client', function () {
   let asyncResult;
   describe('when called with GET_MANY', function () {
-    let ids = [1, 2, 3];
+    const ids = [1, 2, 3];
     beforeEach(function () {
       setupClient();
       asyncResult = aorClient(GET_MANY, 'posts', { ids });
     });
 
     it("calls the client's find method", function () {
-      return asyncResult.then(result => {
+      return asyncResult.then(() => {
         expect(fakeService.find.calledOnce).to.be.true;
       });
     });
 
     it('returns the data returned by the client', function () {
-      return asyncResult.then(result => {
+      return asyncResult.then((result) => {
         expect(result).to.deep.equal(findResult);
       });
     });
 
     it("converts ids in it's params into a query and pass it to client", function () {
       const query = {
-        'id': { '$in': ids },
-        '$limit': ids.length,
+        id: { $in: ids },
+        $limit: ids.length,
       };
-      return asyncResult.then(result => {
+      return asyncResult.then(() => {
         expect(fakeService.find.calledWith({
           query,
         })).to.be.true;
@@ -91,20 +94,20 @@ describe('Rest Client', function () {
   });
 
   describe('when called with GET_MANY_REFERENCE', function () {
-    let params = {
+    const params = {
       pagination: {
         page: 10,
         perPage: 20,
       },
       sort: {
         field: 'id',
-        order: 'DESC'
+        order: 'DESC',
       },
       filter: {
-        name: 'john'
+        name: 'john',
       },
       id: '1',
-      target: '_userId'
+      target: '_userId',
     };
     beforeEach(function () {
       setupClient();
@@ -112,13 +115,13 @@ describe('Rest Client', function () {
     });
 
     it("calls the client's find method", function () {
-      return asyncResult.then(result => {
+      return asyncResult.then(() => {
         expect(fakeService.find.calledOnce).to.be.true;
       });
     });
 
     it('returns the data returned by the client', function () {
-      return asyncResult.then(result => {
+      return asyncResult.then((result) => {
         expect(result).to.deep.equal(findResult);
       });
     });
@@ -129,9 +132,9 @@ describe('Rest Client', function () {
         $skip: 20 * 9,
         '$sort[id]': '-1',
         name: 'john',
-        _userId: '1'
+        _userId: '1',
       };
-      return asyncResult.then(result => {
+      return asyncResult.then(() => {
         expect(fakeService.find.calledWith({
           query,
         })).to.be.true;
@@ -140,21 +143,21 @@ describe('Rest Client', function () {
   });
 
   describe('when called with GET_LIST', function () {
-    let params = {
+    const params = {
       pagination: {
         page: 10,
         perPage: 20,
       },
       sort: {
         field: 'id',
-        order: 'DESC'
+        order: 'DESC',
       },
       filter: {
         name: 'john',
         address: {
-          city: 'London'
-        }
-      }
+          city: 'London',
+        },
+      },
     };
     beforeEach(function () {
       setupClient();
@@ -162,13 +165,13 @@ describe('Rest Client', function () {
     });
 
     it("calls the client's find method", function () {
-      return asyncResult.then(result => {
+      return asyncResult.then(() => {
         expect(fakeService.find.calledOnce).to.be.true;
       });
     });
 
     it('returns the data returned by the client', function () {
-      return asyncResult.then(result => {
+      return asyncResult.then((result) => {
         expect(result).to.deep.equal(findResult);
       });
     });
@@ -179,9 +182,9 @@ describe('Rest Client', function () {
         $skip: 20 * 9,
         '$sort[id]': '-1',
         name: 'john',
-        'address.city': 'London'
+        'address.city': 'London',
       };
-      return asyncResult.then(result => {
+      return asyncResult.then(() => {
         expect(fakeService.find.calledWith({
           query,
         })).to.be.true;
@@ -190,30 +193,30 @@ describe('Rest Client', function () {
   });
 
   describe('id-option: when called with GET_LIST', function () {
-    let params = {
+    const params = {
       pagination: {
         page: 10,
         perPage: 20,
       },
       sort: {
         field: 'id',
-        order: 'DESC'
+        order: 'DESC',
       },
       filter: {
-        name: 'john'
-      }
+        name: 'john',
+      },
     };
 
     it("id-option: calls the client's find method", function () {
-      setupClient({id: '_id'});
+      setupClient({ id: '_id' });
       asyncResult = aorClient(GET_LIST, 'posts', params);
-      return asyncResult.then(result => {
+      return asyncResult.then(() => {
         expect(fakeService.find.calledOnce).to.be.true;
       });
     });
 
     it('id-option: returns the data returned by the client', function () {
-      return asyncResult.then(result => {
+      return asyncResult.then((result) => {
         expect(result).to.deep.equal(findResult);
       });
     });
@@ -223,9 +226,9 @@ describe('Rest Client', function () {
         $limit: 20,
         $skip: 20 * 9,
         '$sort[_id]': '-1',
-        name: 'john'
+        name: 'john',
       };
-      return asyncResult.then(result => {
+      return asyncResult.then(() => {
         expect(fakeService.find.calledWith({
           query,
         })).to.be.true;
@@ -234,30 +237,30 @@ describe('Rest Client', function () {
   });
 
   describe('resource-id-option: when called with GET_LIST', function () {
-    let params = {
+    const params = {
       pagination: {
         page: 10,
         perPage: 20,
       },
       sort: {
         field: 'id',
-        order: 'DESC'
+        order: 'DESC',
       },
       filter: {
-        name: 'john'
-      }
+        name: 'john',
+      },
     };
 
     it("resource-id-option: calls the client's find method", function () {
-      setupClient({posts: {id: '_id'}});
+      setupClient({ posts: { id: '_id' } });
       asyncResult = aorClient(GET_LIST, 'posts', params);
-      return asyncResult.then(result => {
+      return asyncResult.then(() => {
         expect(fakeService.find.calledOnce).to.be.true;
       });
     });
 
     it('resource-id-option: returns the data returned by the client', function () {
-      return asyncResult.then(result => {
+      return asyncResult.then((result) => {
         expect(result).to.deep.equal(findResult);
       });
     });
@@ -267,9 +270,9 @@ describe('Rest Client', function () {
         $limit: 20,
         $skip: 20 * 9,
         '$sort[_id]': '-1',
-        name: 'john'
+        name: 'john',
       };
-      return asyncResult.then(result => {
+      return asyncResult.then(() => {
         expect(fakeService.find.calledWith({
           query,
         })).to.be.true;
@@ -277,15 +280,15 @@ describe('Rest Client', function () {
     });
 
     it("resource-id-option: calls the client's find method for default handled resource", function () {
-      setupClient({widgets: {id: '_id'}});
+      setupClient({ widgets: { id: '_id' } });
       asyncResult = aorClient(GET_LIST, 'posts', params);
-      return asyncResult.then(result => {
+      return asyncResult.then(() => {
         expect(fakeService.find.calledOnce).to.be.true;
       });
     });
 
     it('resource-id-option: returns the data returned by the client for default handled resource', function () {
-      return asyncResult.then(result => {
+      return asyncResult.then((result) => {
         expect(result).to.deep.equal(findResult);
       });
     });
@@ -295,9 +298,9 @@ describe('Rest Client', function () {
         $limit: 20,
         $skip: 20 * 9,
         '$sort[id]': '-1',
-        name: 'john'
+        name: 'john',
       };
-      return asyncResult.then(result => {
+      return asyncResult.then(() => {
         expect(fakeService.find.calledWith({
           query,
         })).to.be.true;
@@ -306,32 +309,32 @@ describe('Rest Client', function () {
   });
 
   describe('when called with GET_ONE', function () {
-    let params = { id: 1 };
+    const params = { id: 1 };
     beforeEach(function () {
       setupClient();
       asyncResult = aorClient(GET_ONE, 'posts', params);
     });
 
     it("calls the client's get method with the id in params", function () {
-      return asyncResult.then(result => {
+      return asyncResult.then(() => {
         expect(fakeService.get.calledOnce).to.be.true;
         expect(fakeService.get.calledWith(1));
       });
     });
 
     it('returns the data returned by the client in a "data" object', function () {
-      return asyncResult.then(result => {
+      return asyncResult.then((result) => {
         expect(result).to.deep.equal({ data: getResult });
       });
     });
   });
 
   describe('when called with UPDATE', function () {
-    let params = {
+    const params = {
       id: 1,
       data: {
-        title: 'updated'
-      }
+        title: 'updated',
+      },
     };
     beforeEach(function () {
       setupClient();
@@ -339,34 +342,33 @@ describe('Rest Client', function () {
     });
 
     it("calls the client's update method with the id and data in params", function () {
-      return asyncResult.then(result => {
+      return asyncResult.then(() => {
         expect(fakeService.update.calledOnce).to.be.true;
         expect(fakeService.update.calledWith(1, { title: 'updated' }));
       });
     });
 
     it('returns the data returned by the client in a "data" object', function () {
-      return asyncResult.then(result => {
+      return asyncResult.then((result) => {
         expect(result).to.deep.equal({ data: updateResult });
       });
     });
   });
 
   describe('when called with UPDATE_MANY', function () {
-
     const params = {
       ids: [1, 2],
       data: {
-        title: 'updated'
-      }
+        title: 'updated',
+      },
     };
 
     describe('when options.usePatch is false', function () {
       const options = { usePatch: false };
-      
+
       beforeEach(function () {
         setupClient(options);
-        asyncResult = aorClient(UPDATE_MANY, "posts", params);
+        asyncResult = aorClient(UPDATE_MANY, 'posts', params);
       });
 
       it('calls the client\'s update method once for each ID', function () {
@@ -376,7 +378,7 @@ describe('Rest Client', function () {
       });
 
       it('returns the ids of the records returned by the client', function () {
-        return asyncResult.then(result => {
+        return asyncResult.then((result) => {
           expect(result).to.deep.equal({ data: params.ids });
         });
       });
@@ -387,7 +389,7 @@ describe('Rest Client', function () {
 
       beforeEach(function () {
         setupClient(options);
-        asyncResult = aorClient(UPDATE_MANY, "posts", params);
+        asyncResult = aorClient(UPDATE_MANY, 'posts', params);
       });
 
       it('calls the client\'s patch method once for each ID', function () {
@@ -397,19 +399,18 @@ describe('Rest Client', function () {
       });
 
       it('returns the ids of the records returned by the client', function () {
-        return asyncResult.then(result => {
+        return asyncResult.then((result) => {
           expect(result).to.deep.equal({ data: params.ids });
         });
       });
     });
-
   });
 
   describe('when called with CREATE', function () {
-    let params = {
+    const params = {
       data: {
-        title: 'created'
-      }
+        title: 'created',
+      },
     };
     beforeEach(function () {
       setupClient();
@@ -417,58 +418,58 @@ describe('Rest Client', function () {
     });
 
     it("calls the client's create method with the data in params", function () {
-      return asyncResult.then(result => {
+      return asyncResult.then(() => {
         expect(fakeService.create.calledOnce).to.be.true;
         expect(fakeService.create.calledWith({ title: 'created' }));
       });
     });
 
     it('returns the data returned by the client in a "data" object', function () {
-      return asyncResult.then(result => {
+      return asyncResult.then((result) => {
         expect(result).to.deep.equal({ data: createResult });
       });
     });
   });
 
   describe('when called with DELETE', function () {
-    let params = { id: 1 };
+    const params = { id: 1 };
     beforeEach(function () {
       setupClient();
       asyncResult = aorClient(DELETE, 'posts', params);
     });
 
     it("calls the client's remove method with the id in params", function () {
-      return asyncResult.then(result => {
+      return asyncResult.then(() => {
         expect(fakeService.remove.calledOnce).to.be.true;
         expect(fakeService.remove.calledWith(1));
       });
     });
 
     it('returns the data returned by the client', function () {
-      return asyncResult.then(result => {
+      return asyncResult.then((result) => {
         expect(result).to.deep.equal({ data: removeResult });
       });
     });
   });
 
-  describe("when called with DELETE_MANY", function () {
-    let params = { ids: [1, 2] };
+  describe('when called with DELETE_MANY', function () {
+    const params = { ids: [1, 2] };
     beforeEach(function () {
       setupClient();
-      asyncResult = aorClient(DELETE_MANY, "posts", params);
+      asyncResult = aorClient(DELETE_MANY, 'posts', params);
     });
 
-    it("calls the client's remove method twice with the ids in params", function() {
-      return asyncResult.then(result => {
+    it("calls the client's remove method twice with the ids in params", function () {
+      return asyncResult.then(() => {
         expect(fakeService.remove.calledTwice).to.be.true;
         expect(fakeService.remove.firstCall.calledWith(1));
         expect(fakeService.remove.secondCall.calledWith(2));
       });
     });
 
-    it("returns the ids of the records returned by the client", function () {
-      return asyncResult.then(result => {
-        expect(result).to.deep.equal({ data: [removeResult['id'], removeResult['id']] });
+    it('returns the ids of the records returned by the client', function () {
+      return asyncResult.then((result) => {
+        expect(result).to.deep.equal({ data: [removeResult.id, removeResult.id] });
       });
     });
   });
@@ -479,13 +480,13 @@ describe('Rest Client', function () {
     });
 
     it('should throw an error', function () {
-      const errorRes = new Error('Unsupported FeathersJS restClient action type WRONG_TYPE')
+      const errorRes = new Error('Unsupported FeathersJS restClient action type WRONG_TYPE');
       try {
         return aorClient('WRONG_TYPE', 'posts', {})
-          .then(result => {
-            throw new Error("client must reject");
+          .then(() => {
+            throw new Error('client must reject');
           })
-          .catch(err => {});
+          .catch(() => {});
       } catch (err) {
         expect(err).to.deep.equal(errorRes);
       }
