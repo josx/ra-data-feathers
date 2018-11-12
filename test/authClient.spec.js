9e3ad2e53d9d67801d16414c3b9fec9898008ccf
Santiago <sgobotta@gmail.com>
2018-11-11 21:42:11 -0300

    AuthClient: Cleans eslint issues in src and test

--- test/authClient.spec.js
+++ test/authClient.spec.js
@@ -1,16 +1,15 @@
-"use strict";
 
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
 const sinon = require('sinon');
+const debug = require('debug');
 const aorAuthClient = require('../lib').authClient;
 
 const { expect } = chai;
 chai.use(chaiAsPromised);
 
-const debug = require('debug')
 
-const dbg = debug('ra-data-feathers:test')
+debug('ra-data-feathers:test');
 
 const fakeClient = {
   // authenticate: ({ email, password }) => { ... }
@@ -19,27 +18,25 @@ const fakeClient = {
 let authClient;
 
 const options = {
-  storageKey: 'storageKey'
+  storageKey: 'storageKey',
 };
 
-function setupClient(options = {}) {
-  authClient = aorAuthClient(fakeClient, options)
+function setupClient(clientOptions = {}) {
+  authClient = aorAuthClient(fakeClient, clientOptions);
 }
-
 
 
 describe('Auth Client', function () {
   describe('when called with AUTH_CHECK', function () {
-
-    describe('when [storageKey] is not set', function() {
-      beforeEach(function() {
+    describe('when [storageKey] is not set', function () {
+      beforeEach(function () {
         setupClient(options);
         global.localStorage = {
-          getItem: sinon.stub().returns()
-        }
+          getItem: sinon.stub().returns(),
+        };
       });
 
-      afterEach(function() {
+      afterEach(function () {
         delete global.localStorage;
       });
 
@@ -47,29 +44,29 @@ describe('Auth Client', function () {
         return expect(authClient('AUTH_CHECK', {})).to.be.rejected;
       });
 
-      describe('when redirectTo is set', function() {
-        let testOptions = Object.assign({}, options, { redirectTo: '/someurl' })
-        beforeEach(function() {
+      describe('when redirectTo is set', function () {
+        const testOptions = Object.assign({}, options, { redirectTo: '/someurl' });
+        beforeEach(function () {
           setupClient(testOptions);
         });
 
-        it('should reject with the redirectTo URL', function() {
+        it('should reject with the redirectTo URL', function () {
           return expect(authClient('AUTH_CHECK', {})).to.be.rejectedWith({ redirectTo: testOptions.redirectTo });
         });
-      })
+      });
     });
 
-    describe('when [storageKey] is set', function() {
-      before(function() {
+    describe('when [storageKey] is set', function () {
+      before(function () {
         global.localStorage = {
-          getItem: sinon.stub().returns('somedata')
-        }
+          getItem: sinon.stub().returns('somedata'),
+        };
       });
-      after(function() {
+      after(function () {
         delete global.localStorage;
       });
 
-      it('should resolve', function() {
+      it('should resolve', function () {
         return expect(authClient('AUTH_CHECK', {})).to.be.fulfilled;
       });
     });
@@ -81,13 +78,13 @@ describe('Auth Client', function () {
     });
 
     it('should throw an error', function () {
-      const errorRes = new Error('Unsupported FeathersJS authClient action type WRONG_TYPE')
+      const errorRes = new Error('Unsupported FeathersJS authClient action type WRONG_TYPE');
       try {
         return authClient('WRONG_TYPE', 'posts', {})
-          .then(result => {
-            throw new Error("client must reject");
+          .then(() => {
+            throw new Error('client must reject');
           })
-          .catch(err => {});
+          .catch(() => {});
       } catch (err) {
         expect(err).to.deep.equal(errorRes);
       }
