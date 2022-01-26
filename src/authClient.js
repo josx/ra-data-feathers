ee477bf933e2fdf41ba9b08c846da8a27b83cafc
Ucok Freddy <ucokfm@gmail.com>
2022-01-27 00:58:38 +0700

    fix: permissionsKey and permissionsField in authClient options types

--- src/authClient.js
+++ src/authClient.js
@@ -8,7 +8,7 @@ import {
 import decodeJwt from 'jwt-decode';
 
 /**
- * @param {{storageKey?: string, authenticate?: {strategy: string}, permissionKey?: string, permissionField?: string, passwordField?: string, usernameField?: string, redirectTo?: string, logoutOnForbidden?: boolean}} options
+ * @param {{storageKey?: string, authenticate?: {strategy: string}, permissionsKey?: string, permissionsField?: string, passwordField?: string, usernameField?: string, redirectTo?: string, logoutOnForbidden?: boolean}} options
  */
 export default (client, options = {}) => (type, params) => {
   const {
